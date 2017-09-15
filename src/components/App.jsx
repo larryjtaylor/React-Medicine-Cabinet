9e42d1f172064096d6c03dee8b55abe95443c7bb
Larry <larryjamestaylor@gmail.com>
2017-09-15 09:10:26 -0700

    build cmoponents directory and add App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function App(props){
+  return (
+    <div>
+      <h1>MediTrack</h1>
+      <h3>Vitamin C</h3>
+      <h3>1 daily</h3>
+      <hr/>
+    </div>
+  );
+}
+
+export default App;
