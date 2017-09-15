23cce53c2a5447fcc4bffc819c755331b39aa124
Larry <larryjamestaylor@gmail.com>
2017-09-15 11:24:07 -0700

    refactor to include conditional statement to show either form or button

--- src/components/NewMedicineControl.jsx
+++ src/components/NewMedicineControl.jsx
@@ -5,18 +5,25 @@ class NewMedicineControl extends React.Component {
   constructor(props) {
     super(props);
     this.state = {formVisibleOnPage: false};
-    this.handleDisplayingNewMedicineForm = this.handleDisplayingNewMedicineForm.bind(this); 
+    this.handleDisplayingNewMedicineForm = this.handleDisplayingNewMedicineForm.bind(this);
   }
   handleDisplayingNewMedicineForm(event){
     this.setState({formVisibleOnPage: true});
   }
 
   render(){
+    let formAreaContent = null;
+      if (this.state.formVisibleOnPage){
+        formAreaContent = <h1>Form will eventually go here!</h1>
+      } else {
+        formAreaContent = <button onClick={this.handleDisplayingNewMedicineForm.bind(this)}>Enter New Medication </button>;
+      }
     return (
-      <button onClick={this.handleDisplayingNewMedicineForm}>Enter New Medication</button>
+      <div>
+        {formAreaContent}
+      </div>
     );
   }
-
 }
 
 export default NewMedicineControl;
