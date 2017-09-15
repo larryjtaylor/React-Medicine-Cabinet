5b2946cecef992cd366414dc1498655137103c99
Larry <larryjamestaylor@gmail.com>
2017-09-15 11:24:34 -0700

    include loop to show multiple instances of medicines

--- src/components/PillsList.jsx
+++ src/components/PillsList.jsx
@@ -1,13 +1,34 @@
 import React from "react";
 import Medicine from "./Medicine";
 
+var medicineList = [
+  {
+    name: "Aspirin",
+    dosage: "2 pills",
+    frequency: "daily"
+  },
+  {
+    name: "Vitamin C",
+    dosage: "1 pill",
+    frequency: "daily"
+  },{
+    name: "Something else",
+    dosage: "2 pills",
+    frequency: "every 12 hours"
+  },
+]
 function PillsList(props){
 
   return (
-    <Medicine
-      name= "Aspirin"
-      dosage= "2 pills"
-      frequency= "daily"/>
+    <div>
+      <hr/>
+        {medicineList.map((medicine, index) =>
+          <Medicine name={medicine.name}
+                  dosage={medicine.dosage}
+                  frequency={medicine.frequency}
+                  key={index}/>
+        )}
+    </div>
   );
 }
 
