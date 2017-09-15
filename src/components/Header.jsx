4a54f19bd9ad2caf5268e1ef986ac51101f5e917
Larry <larryjamestaylor@gmail.com>
2017-09-15 15:54:29 -0700

    add header image and space components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,13 @@ import React from "react";
 function Header(props){
 
     var headerStyle = {
-      textAlign: "center"
+      textAlign: "center",
+      backgroundImage: "url('https://www.etmc.org/wp-content/uploads/2015/09/banner-clinic-family-medicine-generic.jpg')",
+      height: "200px",
+      fontSize: "700%",
+      paddingTop: "50px", 
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat"
     }
 
   return (
